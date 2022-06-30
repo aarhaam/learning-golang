6c5b221c4d7e27236367c7514bc616de8b3747c0
dika <mahardikastudioh@gmail.com>
2022-06-30 17:43:18 +0800

    day one

---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhammad")
+	fmt.Println("Kharisma Mahardika")
+
+	fmt.Println(len("Muhammad"))
+	fmt.Println(("Kharisma Mahardika")[0]) //representasi bytes K
+}
